3db3b40287320211137c5dc67e71ab837c7c782f
Joey Gaitan <joeygaitan707@gmail.com>
2019-03-22 04:32:10 -0700

    installed npm packages, finished file structure, now working on login page

---
+++ src/components/LoginPage/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            Admin: "123"
+         }
+    }
+    render() { 
+        return ( 
+        <div>
+            
+        </div> );
+    }
+}
+ 
+export default Login;

f70c08e62ca9e7892c275c5eaf29e7bfadef1754
Joey Gaitan <joeygaitan707@gmail.com>
2019-04-05 17:48:35 -0700

    working on getting the ading posts working

--- src/components/LandingPage/Posts/AddPost.js
+++ src/components/LandingPage/Posts/AddPost.js
@@ -8,12 +8,12 @@ class AddPost extends Component {
                 author: "",
                 content:"",
                 title: "",
-                img_url: ""
+                img_url: "",
+                empty: true
          }
     }
 
-    change = (event) =>{
-        console.log(event.target.value)
+    onUpdate = (event) =>{
         this.setState({
            [event.target.name] : event.target.value
         })
@@ -26,35 +26,39 @@ class AddPost extends Component {
         this.props.history.push('/posts')
       }
 
-      refresh = () => {
-        this.props.history.push('/posts') 
+      ifEmpty = () => {
+        if(this.state.author === "" || this.state.content === '' || this.state.title === '' || this.state.img_url === ''){
+            
+        }else{
+            this.setState({
+                empty: false
+            })
+        }
       }
-      
 
     render() { 
         console.log(this.props)
         return ( 
         <div className="">
-        <form className="form" onSubmit={this.updatedSubmit}>
-                        <div className="">
-                            <label className>Author</label>
-                            <input name ="author" type="text" className="" value={this.state.author} onChange={this.onUpdate}></input>
-                        </div>
-                        <div className="">
-                            <label>content</label>
-                            <input name ="content" type="text" className="" value={this.state.content} onChange={this.onUpdate}></input>
-                        </div>
-                        <div className="">
-                            <label>title</label>
-                            <input name ="title" type="text" className="" value={this.state.title} onChange={this.onUpdate}></input>
-                        </div>
-                        <div className="">
-                            <label>published</label>
-                            <input name ="published" type="text" className="" value={this.state.published} onChange={this.onUpdate}></input>
-                        </div>
-                        <button type="submit" className="">Create New Post</button>
-        </form>
-            <button onClick={()=>this.refresh()}>Cancel</button>
+            <form className="form" onSubmit={this.updatedSubmit}>
+                            <div className="">
+                                <label className>Author</label>
+                                <input name ="author" type="text" className="" value={this.state.author} onChange={this.onUpdate}></input>
+                            </div>
+                            <div className="">
+                                <label>Description</label>
+                                <input name ="content" type="text" className="" value={this.state.content} onChange={this.onUpdate}></input>
+                            </div>
+                            <div className="">
+                                <label>Title</label>
+                                <input name ="title" type="text" className="" value={this.state.title} onChange={this.onUpdate}></input>
+                            </div>
+                            <div className="">
+                                <label>Image Url</label>
+                                <input name ="img_url" type="text" className="" value={this.state.img_url} onChange={this.onUpdate}></input>
+                            </div>
+                            {this.state.empty ? <button className="clearedOut" disabled>Create New Post</button> : <button type="submit" className="">Create New Post</button>}
+            </form>
         </div> );
     }
 }
